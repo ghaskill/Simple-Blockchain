88f7e55a5d65c4d34a3a452223af09764178d2a2
ghaskill <ghaskill@gmail.com>
2022-05-31 18:11:34 -0400

    save and recommit

--- blockchain.py
+++ blockchain.py
@@ -1,20 +1,67 @@
+import requests
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
   def __init__(self):
     self.chain = []
     self.current_transactions = []
 
-  def new_block(self):
-    # Creates and adds new block to chain
-    pass
+    # Create genesis block
+    self.new_block(previous_hash=1, proof=100)
 
-  def new_transaction(self):
+  def new_block(self, proof, previous_hash=None):
+    """
+    Creates and adds new block to chain
+    :param proof: <int> proof given by Proof of Work algorithm
+    :param previous_hash: (Optional) <str> Hash of previous block
+    :return: <dict> New Block
+    """
+
+    block = {
+      'index': len(self.chain) + 1,
+      'timestamp': time(),
+      'transactions': self.current_transactions,
+      'proof': proof,
+      'previous_hash': previous_hash or self.hash(self.chain(-1))
+    }
+
+    # Reset current list of transactions
+    self.current_transactions = []
+
+    self.chain.append(block)
+    return block
+
+  def new_transaction(self, sender, recipient, amount):
     # Adds transaction to list of transactions
-    pass
+    """
+    Creates a new transaction
+    :param sender: <str> Address of sender
+    :param recipient: <str> Address of recipient
+    :param amount: <int> Amount
+    :return: <int> Index of block that will hold this transaction
+    """
+
+    self.current_transactions.append({
+      'sender': sender,
+      'recipient': recipient,
+      'amount': amount,
+    })
+
+    return self.last_block['index'] + 1
 
   @staticmethod
   def hash(block):
-    # Hashes a block
-    pass
+    """
+    Creates a Sha-256 hash of block
+    :param block: <dict> block
+    :return: <str>
+    """
+
+    # Ensures dictionary is ordered
+    block_string = json.dumps(block, sort_keys=True).encode()
+    return hashlib.sha256(block_string).hexdigest()
 
   @property
   def last_block(self):
